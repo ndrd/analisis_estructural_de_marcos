76c7b10d0e500153a8bfb3d04023210e4adee0d0
Jonathan de Jesús Andrade López <ndrd@ciencias.unam.mx>
2015-05-31 16:54:12 -0500

    fuerzas de empeoramiento

--- analisis_de_marcos.py
+++ analisis_de_marcos.py
@@ -5,6 +5,7 @@ import numpy as np
 import sys
 import math
 from pprint import pprint as pp
+import json
 
 nodos = []
 barras = []
@@ -99,12 +100,15 @@ class Barra(object):
 
 		self.FxLocal = float(_datos[9])
 		self.FyLocal = float(_datos[10])
+		self.distancia_fuerza_externa = float(_datos[11])
 
 		self.crear_matriz_transformacion()
-		self.crear_matriz_desconocida()
-		self.crear_matriz_K_rigidez()
+		self.crear_matriz_rigidez_elemento()
+		self.crear_matriz_K_ceros()
+		self.calcular_fuerzas_empotramiento_en_y()
 
-	def crear_matriz_transformacion():
+
+	def crear_matriz_transformacion(self):
 
 		puntoA = self.puntoA
 		puntoB = self.puntoB
@@ -114,14 +118,82 @@ class Barra(object):
 		C = ( puntoB.x - puntoA.x ) / L
 
 		self.matriz_transformada = [
-			[ C,-S, 0],
-			[ S, C, 0],
+			[ C, S, 0],
+			[-S, C, 0],
 			[ 0, 0, 1]
 		]
 
 		self.matriz_transformada_transpuesta =  transpose(self.matriz_transformada)
 		self.L = L
 
-	def crear_matriz_desconocida():
+	def crear_matriz_rigidez_elemento(self):
+		#Matriz de fuerza de cada nudo
+		self.Km = []
+		EA = self.E * self.A
+		EI = self.E * self.I
+		L = self.L
 
+		self.Km.append([  EA/L    ,      0      ,     0       ,   -EA/L    ,      0      ,     0      ])
+		self.Km.append([    0     , 12*EI/L**3  , 6*EI/L**2   ,     0      , -12*EI/L**3 ,  6*EI/L**2 ])
+		self.Km.append([    0     , 6*EI/L**2   , 4*EI/L      ,     0      , -6*EI/L**2  ,  2*EI/L    ])
+		self.Km.append([  -EA/L   ,      0      ,     0       ,    EA/L    ,      0      ,     0      ])
+		self.Km.append([    0     , -12*EI/L**3 , -6*EI/L**2  ,     0      , 12*EI/L**3  , -6*EI/L**2 ])
+		self.Km.append([    0     , 6*EI/L**2   , 2*EI/L      ,     0      , -6*EI/L**2  ,  4*EI/L    ])
+
+	def crear_matriz_K_ceros(self):
+
+		ceros = [
+			[0,0,0],
+			[0,0,0],
+			[0,0,0],
+		]
+
+	def calcular_fuerzas_empotramiento_en_y(self):
 		
+		a = self.distancia_fuerza_externa
+		b = self.L - a
+		P = self.FyLocal 
+		L = self.L
+
+		print  "Pa", self.puntoA.x , " L:", L, " a:", a , " b:", b
+
+		M1 = ( P *  a * b ** 2 ) / L ** 2
+		M2 = (-P *  a ** 2 * b ) / L ** 2
+
+		Fy1 = ((P * b ** 2 ) / L ** 3 ) * (3 * a + b)
+		Fy2 = ((P * a ** 2 ) / L ** 3 ) * (3 * b + a)
+
+		print "Fy1" , " ", Fy1
+		print "Fy2" , " ", Fy2
+		print "M1" , " ", M1
+		print "M2" , " ", M2
+
+
+def mostrar_nodos():
+	for nodo in nodos:
+		print json.dumps(nodo.__dict__)
+
+def mostrar_barras():
+	for barra in barras:
+		print json.dumps(barra.__dict__)
+
+
+
+
+def main(argv):
+	datos = [linea.rstrip('\n').split() for linea in open(argv[1])]
+
+	for _dato in datos:
+
+		if len(_dato) != 0:
+			if _dato[0] == 'nodo':
+				nodos.append(Nodo(_dato))
+			elif _dato[0] == 'barra':
+				barras.append(Barra(_dato))
+
+	mostrar_nodos()
+	#mostrar_barras()
+
+
+if __name__ == '__main__':
+	main(sys.argv)		
